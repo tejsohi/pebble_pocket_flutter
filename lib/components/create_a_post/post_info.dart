ae44cb413f679efbf56ddf443239384f35bd49cd
tejsohi <tejsohi@hotmail.co.uk>
2023-03-30 11:53:37 +0100

    Added icons for My Assets page

--- lib/components/create_a_post/post_info.dart
+++ lib/components/create_a_post/post_info.dart
@@ -8,31 +8,35 @@ class PostInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Icon(Icons.image),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           children: [
-            Text(
-              post.postTitle,
-              style: TextStyle(fontSize: 18),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset('content/images/icons/post.png'),
             ),
-            Text(
-              'Created: Friday at 00:00pm',
-              style: TextStyle(fontSize: 12),
-            ),
-            Divider(
-              height: 0,
-              thickness: 1,
-              indent: 5,
-              endIndent: 5,
-              color: Colors.grey,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  post.postTitle,
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  'Created: Friday at 00:00pm',
+                  style: TextStyle(fontSize: 12),
+                ),
+              ],
             ),
           ],
+        ),
+        Divider(
+          height: 0,
+          thickness: 1,
+          indent: 5,
+          endIndent: 5,
+          color: Colors.grey,
         ),
       ],
     );
